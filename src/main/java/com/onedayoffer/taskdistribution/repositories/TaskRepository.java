5ad2b5e5da6c6ba53b1a9e80691eae9a4bda9f15
KIlin <kilin@sportmaster.ru>
2024-03-23 02:05:57 +0300

    fix

--- src/main/java/com/onedayoffer/taskdistribution/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onedayoffer.taskdistribution.repositories;
-
-import com.onedayoffer.taskdistribution.repositories.entities.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
